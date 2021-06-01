fd7cd589d360bf4612a19c1953744670a2a8d7b8
umairkhalid1997 <umairkhalidtts119@gmail.com>
2021-06-01 11:06:36 +0500

    ok

---
+++ app.js
@@ -0,0 +1,76 @@
+// Question NUmber 1 (for positive value)
+// var num = Number(prompt("Enter number"));
+// var roundOfNumber = Math.round(num);
+// var floorNumber = Math.floor(num);
+// var ceilNumber = Math.ceil(num);
+// document.write("<br>" + "Number: " + num);
+// document.write("<br>" + "Round off value: " + roundOfNumber);
+// document.write("<br>" + "Floor value: " + floorNumber);
+// document.write("<br>" + "Ceil value: " + ceilNumber);
+
+// Question NUmber 2 (for negative value)
+// var num = Number(prompt("Enter number"));
+// var roundOfNumber = Math.round(num);
+// var floorNumber = Math.floor(num);
+// var ceilNumber = Math.ceil(num);
+// document.write("<br>" + "Number: " + num);
+// document.write("<br>" + "Round off value: " + roundOfNumber);
+// document.write("<br>" + "Floor value: " + floorNumber);
+// document.write("<br>" + "Ceil value: " + ceilNumber);
+
+// Question NUmber 3
+// var number = Number(prompt("Enter any number"));
+// var absoluteNumber = Math.abs(number);
+// document.write("<br>"+ "The absolute value of " + number +" is " + absoluteNumber);
+
+// Question NUmber 4
+// var number = Math.random();
+// number = (number*6)+1;
+// number = Math.floor(number);
+// document.write("<br>" + "Random dice value: " + number);
+
+//0r
+
+// var number = Math.random();
+// number = (number*6);
+// number = Math.ceil(number);
+// document.write("<br>" + "Random dice value: " + number);
+
+// Question NUmber 5
+// var coin = Math.random();
+// coin = (coin*2);
+// coin = Math.ceil(coin);
+// if (coin === 2){
+//     document.write("<br>"+ coin);
+//     document.write("<br>"+ "Random coin value: Heads");
+// }
+// else{
+//     document.write("<br>"+ coin);
+//     document.write("<br>"+ "Random coin value: Tails");
+// }
+
+// Question NUmber 6
+// var number = Math.random();
+// number = (number * 99);
+// number = Math.ceil(number);
+// if (number === 1) {
+//     document.write("<br>" + "Random number between 1 to 100: " + number + 1)
+// }
+// else {
+
+//     document.write("<br>" + "Random number between 1 to 100: " + number)
+// }
+
+// Question NUmber 7
+// var weight = prompt("Enter your weight");
+// document.write("<br>" + "The weight of user is "+ weight);
+
+// Question NUmber 8
+// var secretNumber = Math.ceil(Math.random() * 10);
+// var guesNumber = Number(prompt("Enter number between number 1 to 10"));
+// if (secretNumber === guesNumber) {
+//     document.write("<br>" + "Congrats! You are right");
+// }
+// else{
+//     document.write("<br>" + "Try again");
+// }
